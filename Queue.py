8dc9ffbb0f2ed6c48c77ccf14509e989066164e8
zzaakiirr <zzaakiirr@gmail.com>
2018-09-16 16:15:56 +0300

    Declared OffsetIsOutQueueException class + Added raising this exception to get_message() method of queue + Declared get_message_in_queue(queue_instance) function

--- Queue.py
+++ Queue.py
@@ -1,8 +1,23 @@
-class Queue:
-    def get_message(offset):
+class OffsetIsOutQueueException(Exception):
+    pass
+
+
+class Queue():
+    def __init__(self, messages):
+        self.messages = messages
+        self.messages_count = len(self.messages)
+
+    def get_message(self, offset):
+        if offset > self.messages_count:
+            raise OffsetIsOutQueueException
         pass
 
 
+def get_messages_in_queue(queue_instance):
+    pass
+
+
 if __name__ == '__main__':
-    for message in messages_in_queue(queue_instance):
+    messages_in_queue = get_messages_in_queue(queue_instance)
+    for message in messages_in_queue:
         print(message)
